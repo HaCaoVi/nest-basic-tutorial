02a82f4c07bb7cf7883c6cfa45c1c2e47f4229e1
Ha Cao Vi <icaovy2001@gmail.com>
2025-09-09 16:07:06 +0700

    done jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
-import { JwtAuthGuard } from './auth/passport/jwt-auth.guard';
+import { JwtAuthGuard } from './modules/auth/passport/jwt-auth.guard';
 
 @Module({
   imports: [
--- src/common/interfaces/authenticated-request.interface.ts
+++ src/common/interfaces/authenticated-request.interface.ts
@@ -1,5 +1,5 @@
 import { Request } from "express";
-import { User } from "src/users/schemas/user.schema";
+import { User } from "src/modules/users/schemas/user.schema";
 
 export interface AuthenticatedRequest extends Request {
     user: Omit<User, 'password'>;
