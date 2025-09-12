2de5cf87b83d3a2fc857a56981784ab9fded48ab
Ha Cao Vi <icaovy2001@gmail.com>
2025-09-12 23:52:09 +0700

    config versioning for api

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { JwtAuthGuard } from '@modules/auth/passport/jwt-auth.guard';
 import { TransformInterceptor } from '@common/interceptors/transform.interceptor';
 
@@ -18,6 +18,12 @@ async function bootstrap() {
     "origin": `${configService.get("FE_ORIGIN_URL")}`,
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "credentials": "true",
+  });
+
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: ['1', '2'],
   });
 
   await app.listen(configService.get('PORT') ?? 3000);
