66e448a3de884c967422050cdcaa67860818a83f
HaCaoVi <icaovy2001@gmail.com>
2025-09-18 16:01:29 +0700

    CRUD permission

--- src/modules/permissions/permissions.controller.ts
+++ src/modules/permissions/permissions.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { PermissionsService } from './permissions.service';
 import { CreatePermissionDto } from './dto/create-permission.dto';
 import { UpdatePermissionDto } from './dto/update-permission.dto';
 import { ResponseMessage, User } from '@common/decorators/customize.decorator';
 import type { IInfoDecodeToken } from '@common/interfaces/customize.interface';
+import { ParseObjectIdPipe } from '@nestjs/mongoose';
 
 @Controller('permissions')
 export class PermissionsController {
@@ -19,22 +20,33 @@ export class PermissionsController {
   }
 
   @Get()
-  findAll() {
-    return this.permissionsService.findAll();
+  findAll(
+    @Query() query: any
+  ) {
+    const { current, pageSize, ...filters } = query;
+    return this.permissionsService.findAll(+current, +pageSize, filters);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.permissionsService.findOne(+id);
+  findOne(@Param('id', ParseObjectIdPipe) id: string) {
+    return this.permissionsService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePermissionDto: UpdatePermissionDto) {
-    return this.permissionsService.update(+id, updatePermissionDto);
+  @ResponseMessage("Updated Successfully")
+  update(
+    @User() user: IInfoDecodeToken,
+    @Param('id', ParseObjectIdPipe) id: string,
+    @Body() updatePermissionDto: UpdatePermissionDto
+  ) {
+    return this.permissionsService.update(user, id, updatePermissionDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.permissionsService.remove(+id);
+  @ResponseMessage("Deleted Successfully")
+  remove(
+    @User() user: IInfoDecodeToken,
+    @Param('id', ParseObjectIdPipe) id: string) {
+    return this.permissionsService.remove(user, id);
   }
 }
--- src/modules/permissions/permissions.service.ts
+++ src/modules/permissions/permissions.service.ts
@@ -1,10 +1,11 @@
 import { BadRequestException, HttpException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { CreatePermissionDto } from './dto/create-permission.dto';
 import { UpdatePermissionDto } from './dto/update-permission.dto';
-import type { IInfoDecodeToken } from '@common/interfaces/customize.interface';
+import type { IInfoDecodeToken, PaginatedResult } from '@common/interfaces/customize.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { Permission } from './schemas/permission.schema';
 import type { PermissionModelType } from './schemas/permission.schema';
+import { normalizeFilters } from '@common/helpers/convert.helper';
 
 @Injectable()
 export class PermissionsService {
@@ -41,19 +42,84 @@ export class PermissionsService {
     }
   }
 
-  findAll() {
-    return `This action returns all permissions`;
+  async findAll(current: number, pageSize: number, filters: Record<string, any> = {}): Promise<PaginatedResult<Permission>> {
+    try {
+      if (!current) current = 1
+      if (!pageSize) pageSize = 10
+
+      const { sort, ...filter } = filters
+
+      const skip = (current - 1) * pageSize;
+
+      const [totalItems, result] = await Promise.all([
+        this.permissionService.countDocuments(normalizeFilters(filter)),
+        this.permissionService
+          .find(normalizeFilters(filter))
+          .skip(skip)
+          .limit(pageSize)
+          .sort(sort)
+          .lean<Permission[]>()
+          .exec()
+      ]);
+
+      const totalPages = Math.ceil(totalItems / pageSize);
+
+      return {
+        meta: {
+          current,
+          pageSize,
+          pages: totalPages,
+          total: totalItems,
+        },
+        result,
+      };
+    } catch (error) {
+      this.logger.error(error.message, error.stack);
+      if (error instanceof HttpException) throw error;
+      throw new InternalServerErrorException('Something went wrong!');
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} permission`;
+  async findOne(id: string): Promise<Permission> {
+    try {
+      const permission = await this.permissionService
+        .findById(id)
+        .lean<Permission>()
+        .exec();
+      if (!permission) throw new NotFoundException(`Permission with id ${id} not found`);
+      return permission;
+    } catch (error) {
+      this.logger.error(error.message, error.stack);
+      if (error instanceof HttpException) throw error;
+      throw new InternalServerErrorException('Something went wrong!');
+    }
   }
 
-  update(id: number, updatePermissionDto: UpdatePermissionDto) {
-    return `This action updates a #${id} permission`;
+
+  async update(user: IInfoDecodeToken, id: string, updatePermissionDto: UpdatePermissionDto) {
+    try {
+      const dataConfig = { ...updatePermissionDto }
+      const updated = await this.permissionService.updateOne({ _id: id }, { ...dataConfig, updatedBy: user._id }, { runValidators: true })
+      if (!updated) throw new NotFoundException(`Permission with id ${id} not found`);
+      return updated;
+    } catch (error) {
+      this.logger.error(error.message, error.stack);
+      if (error instanceof HttpException) throw error;
+      throw new InternalServerErrorException('Something went wrong!');
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} permission`;
+  async remove(user: IInfoDecodeToken, id: string) {
+    try {
+      const result = await this.permissionService.softDeleteOne({ _id: id }, user._id)
+      if (result.matchedCount === 0) throw new NotFoundException(`Permission with id ${id} not found`);
+      return {
+        deleted: "ok"
+      };
+    } catch (error) {
+      this.logger.error(error.message, error.stack);
+      if (error instanceof HttpException) throw error;
+      throw new InternalServerErrorException('Something went wrong!');
+    }
   }
 }
